bb54c54d264ae5a76b11ee6e474e1d65273d65b3
Caryzzou <caryzzou@gmail.com>
2023-01-20 10:37:30 +0800

    Update upload_jsons.sh

--- upload_jsons.sh
+++ upload_jsons.sh
@@ -1,1 +1,1 @@
-scp -i ~/aws_zouzan_sg.pem ./*.json root@18.136.87.62:/var/www/vserver/configs/uploaded/
+scp -i ~/aws_zouzan_sg.pem ./*config.json ./*servers.json root@18.136.87.62:/var/www/vserver/configs/uploaded/
