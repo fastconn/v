3616575347b765a02f0782397cba8718e14757c8
Caryzzou <caryzzou@gmail.com>
2023-02-07 23:11:47 +0800

    rm sh

--- upload_release_jsons.sh
+++
@@ -1,1 +0,0 @@
-scp -i ~/aws_zouzan_sg.pem ./*config.json ./*servers.json root@18.136.87.62:/var/www/vserver/configs/
--- upload_test_jsons.sh
+++
@@ -1,1 +0,0 @@
-scp -i ~/aws_zouzan_sg.pem ./*_t.json root@18.136.87.62:/var/www/vserver/configs/
